e7f8f29d9fa54d106b4125049e2a8e4a32f003d4
NeiloErnesto89 <neil.smyth1989@hotmail.com>
2023-05-07 22:37:52 +0100

    added navbar and icons with cdn on base.html page, updating the navigation a bit and working buttons too

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views # import views module from current directory
 #special var -> array of urlpattern objects
 # views we dont pass function, give just a reference 
 # URLCOnf module -> URL Configuration and we want to import this into the main url configuration
-urlpatterns = [ path('', views.index), # empty string -> root of our app') 
+urlpatterns = [ path('', views.index, name='home_page'), # empty string -> root of our app') 
                path('hello/', views.say_hello), 
                path('temp/', views.template_test),
                path('geo/', views.geolocation ),
