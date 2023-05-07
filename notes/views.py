4a82efd564d03ebeeaa451622ae59e5263d80138
NeiloErnesto89 <neil.smyth1989@hotmail.com>
2023-05-07 17:49:01 +0100

    update notes.py

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404 # return correct error msg
 
 from .models import Notes
 
@@ -9,5 +10,9 @@ def notes_list(request):
 
 # test to display admin note content in template - extra arg 'pk' for primary key to id note object
 def notes_detail(request, pk):
-    current_note = Notes.objects.get(pk=pk) # get note object from db, supply pk to get specific note
+    # wrap query in try except block to catch error if note doesn't exist
+    try:
+        current_note = Notes.objects.get(pk=pk) # get note object from db, supply pk to get specific note
+    except Notes.DoesNotExist: # object doesn't exist
+        raise Http404("Note does not exist, please try another") 
     return render(request, 'notes/notes_detail.html', {'note': current_note}) # return request, template, context
