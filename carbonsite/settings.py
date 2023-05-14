1b1a0d69a98ab069417a03ef4575928f8e4f0776
NeiloErnesto89 <neil.smyth1989@hotmail.com>
2023-05-14 23:21:57 +0100

    minor update before env.py test and add .gitignore file

--- carbonsite/settings.py
+++ carbonsite/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+# secret key test
+# from os import path
+# if path.exists("env.py"):
+#   import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+# print(SECRET_KEY + " is the secret key")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
