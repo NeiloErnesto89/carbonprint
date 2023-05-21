dd9d72a5ed0c8344269e60c2ce09acee60ae04f3
NeiloErnesto89 <neil.smyth1989@hotmail.com>
2023-05-21 17:04:26 +0100

    updated admin notes section, non user redirected to login, users redirect to profile when trying to create or delete and admin has full access - with messages

--- notes/forms.py
+++ notes/forms.py
@@ -5,4 +5,5 @@ from .models import Notes
 class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
+        # ordering = ['-id']
         fields = ('title', 'content')
--- notes/views.py
+++ notes/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger # impor
 
 # class base views
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.decorators import login_required # decorator to restrict access to view
+from django.contrib.auth.decorators import login_required, user_passes_test # staff_member_required # decorator to restrict access to view
 
 from .models import Notes
 from .forms import NotesForm    
@@ -14,6 +14,7 @@ from .forms import NotesForm
 
 # we want all the notes info (objects) coming from the database to be avaialble to the template
 # added pagination to notes_list view - https://docs.djangoproject.com/en/4.2/topics/pagination/
+@login_required(login_url='login') 
 def notes_list(request):
     all_notes = Notes.objects.all() # get all notes from db
     # Paginator class to split notes into pages
@@ -34,6 +35,7 @@ def notes_list(request):
     
     
 # test to display admin note content in template - extra arg 'pk' for primary key to id note object
+@login_required(login_url='login') 
 def notes_detail(request, pk):
     # wrap query in try except block to catch error if note doesn't exist
     try:
@@ -49,7 +51,8 @@ def notes_detail(request, pk):
 #     fields = ['title', 'content'] # fields we want to display in form
 #     success_url = 'internal/notes' # url we want to redirect to after form submission
 
-# @login_required
+# @login_required(login_url='login') 
+@user_passes_test(lambda u: u.is_superuser, login_url='profile') # lambda to test if user is superuser and decorator to restrict access to view
 def notes_create(request, pk=None):
     post = get_object_or_404(Notes, pk=pk) if pk else None
     if request.method == 'POST':
@@ -63,11 +66,13 @@ def notes_create(request, pk=None):
         return render(request, 'notes/notes_form.html', {'form': form})
     # return render(request, 'notes/create_note.html')
     
-
+@login_required(login_url='login') 
 def delete_note(request, pk):
     note = get_object_or_404(Notes, pk=pk)
     if request.user.is_authenticated and request.user.is_superuser: # is user is authenticated and is superuser/admin
         note.delete()
         messages.success(request, 'Your post has been deleted')
+    else: 
+        messages.error(request, 'You do not have permission to delete this post') # normal user
     return redirect('notes_list')
     
